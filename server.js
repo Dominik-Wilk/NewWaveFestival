07ac51325f5e8d24191e069b38cfbd1191de0a1a
Dominik Wilk <mr.wilkd@gmail.com>
2023-12-12 13:35:51 +0100

    Prepare files to public project

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonial.routes');
 const concertsRoutes = require('./routes/concert.routes');
 const seatsRoutes = require('./routes/seat.routes');
+
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(
   cors({
     origin: 'http://localhost:3000',
   })
 );
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
